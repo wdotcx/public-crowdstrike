43fe910ac4f4784f15de223f9e3dfa3b8af7c8af
Paul Webb <bsdnet@gmail.com>
2024-05-29 11:42:04 +1000

    [UPDATE] load .env credentials in watch mode script

--- API_WatchMode.ps1
+++ API_WatchMode.ps1
@@ -1,21 +1,57 @@
 #
 # 'API_WatchMode.ps1' Quickly enable or disable CrowdStike Watch Mode on firewall rules within a rule-group.
 #
+# .env
+# CLIENT_ID=
+# CLIENT_SECRET=
+#
 Import-Module PSFalcon
 
 #
 # Variables
 #
-$ClientId = ''
-$ClientSecret = ''
 $FalconFirewallGroup = ''
 
 $WatchMode = '1'    # 1 - Enabled or 0 - Disabled
-$WatchModeRuleId = @(1, 4, 5, '4-10', 17, 34)    # CrowdStrike rule number or range e.g. (1, 4, 5, '4-10', 17, 34)
+$WatchModeRuleId = @(1, 2)    # CrowdStrike rule number or range e.g. (1, 4, 5, '4-10', 17, 34)
+
+$ClientId = ''    # .env credential override
+$ClientSecret = ''    # .env credential override
 
 #
 # Main
 #
+$envFilePath = ".env"
+
+function Get-EnvVars {
+    param (
+        [string]$FilePath
+    )
+
+    $envVars = @{}
+    if (Test-Path $FilePath) {
+        $lines = Get-Content $FilePath
+        foreach ($line in $lines) {
+            if ($line -match '^\s*([^#][^=]+)\s*=\s*(.*)\s*$') {
+                $key = $matches[1].Trim()
+                $value = $matches[2].Trim()
+                $envVars[$key] = $value
+            }
+        }
+    }
+    return $envVars
+}
+
+if (Test-Path $envFilePath) {
+    $envVars = Get-EnvVars -FilePath $envFilePath
+    if ($envVars.ContainsKey('CLIENT_ID')) {
+        $ClientId = $envVars['CLIENT_ID']
+    }
+    if ($envVars.ContainsKey('CLIENT_SECRET')) {
+        $ClientSecret = $envVars['CLIENT_SECRET']
+    }
+}
+
 Request-FalconToken -ClientId $ClientId -ClientSecret $ClientSecret
 
 $rangeArray = @()
